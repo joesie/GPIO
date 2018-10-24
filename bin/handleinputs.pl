b97fc1b1bc37920aa10bb69180c00727e63b66a6
Jörg Siedhof <jsiedhof@siedhof.de>
2018-10-25 00:05:39 +0200

    Prefix handling

--- bin/handleinputs.pl
+++ bin/handleinputs.pl
@@ -15,10 +15,12 @@ my $log = LoxBerry::Log->new(name => 'Input_handler',);
 
 
 
-LOGSTART("starts Handle input daemon");
+LOGSTART("Handle input daemon");
 
 my $pcfg = new Config::Simple("$lbpconfigdir/pluginconfig.cfg");
+my $prefix = $pcfg->param("inputs.prefix");
 
+LOGERR "$prefix";
 
 #endless loop
 while(1){
@@ -29,20 +31,26 @@ while(1){
 	    
 	    my $response;
 	    if($value == 0){
-	    		$response = LoxBerry::IO::mshttp_send_mem(1, "input$i", "Off");
+	    		$response = LoxBerry::IO::mshttp_send_mem(1, "$prefix$i", "Off");
 	    			
 	    } else {
-	    		$response = LoxBerry::IO::mshttp_send_mem(1, "input$i", "On");
+	    		$response = LoxBerry::IO::mshttp_send_mem(1, "$prefix$i", "On");
 	    }
 	    
 		if (! $response) {
-		    LOGERR "Error sending to Miniserver";
+		    LOGERR "Error sending to Miniserver $response";
 		} else {
-		    LOGDEB "Send ok Input$i: $value";
+		    LOGDEB "Send ok $prefix$i: $value";
 		}
 	}
+	#wenn der Loglevel mehr als Fehler ist (z.b. Debug) wird die Pollzeit aus
+	#Sicherheitsgruenden fest auf 1s fest gesetzt 
+	if($log->loglevel() >3){
+		sleep(1);
+	} else {
+		sleep (0.1);
+	}
 	
-	sleep (0.1);
 }
 
 exit;
