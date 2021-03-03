630a154be1123633e0c4105baabb3e0e4e320c7e
Jörg Siedhof <jsiedhof@siedhof.de>
2021-03-03 15:19:18 +0100

    current working copy

--- bin/inputHandler.py
+++ bin/inputHandler.py
@@ -20,50 +20,90 @@ import json
 from threading import Thread
 SleepTimeL = 0.2
 
-with open('/opt/loxberry/config/plugins/gpio/pluginconfig.json') as json_pcfg_file:
-    pcfg = json.load(json_pcfg_file)
-    gpio = pcfg['gpio'];
-    for channel in gpio['inputs']:
-        print('pin: ' + p['pin'])
-        print('wireing: ' + p['wiring'])
+#Configuration MQTT Topics
+MQTT_SERVER             = "localhost"
+#Output
+MQTT_PATH_STATE_OUTPUT  = "gpio/Output/Pin_"
+MQTT_PATH_CONFIG	    = "gpio/Configuration"
+#Input
+MQTT_PATH_STATE_INPUT   = "gpio/Input/Pin_"
 
-
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 # ============================
 def callback_input(channel):
  now = datetime.datetime.now()
 
  if GPIO.input(channel): # if SENSOR_PIN of channel == 1 or high
-   #print now.strftime('%Y-%m-%d %H:%M:%S') + " : " + (MQTT_PATH_STATE_INPUT + str(channel) + ': ON')
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/state" , "1", hostname=MQTT_SERVER)
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/stateText" , "ON", hostname=MQTT_SERVER)
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/timestamp_ON", now.strftime('%Y-%m-%d %H:%M:%S'), hostname=MQTT_SERVER)
-   print (str(channel) + "high")
-   time.sleep(SleepTimeL);
- else: # if SENSOR_PIN of channel != 1 or low
-   #print now.strftime('%Y-%m-%d %H:%M:%S') + " : " + (MQTT_PATH_STATE_INPUT + str(channel) + ': OFF')
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/stateText", "OFF", hostname=MQTT_SERVER)
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/state", "0", hostname=MQTT_SERVER)
-   # publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/timestamp_OFF", now.strftime('%Y-%m-%d %H:%M:%S'), hostname=MQTT_SERVER)
-   print (str(channel) + "low")
+   print now.strftime('%Y-%m-%d %H:%M:%S') + " : " + (MQTT_PATH_STATE_INPUT + str(channel) + ': ON')
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/state" , "1", hostname=MQTT_SERVER)
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/stateText" , "ON", hostname=MQTT_SERVER)
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/timestamp_ON", now.strftime('%Y-%m-%d %H:%M:%S'), hostname=MQTT_SERVER)
+
    time.sleep(SleepTimeL);
 
-def main():
-    while 1:
-        print ("while loop active")
-        time.sleep(10);
-# client = mqtt.Client()
-#  client.on_connect = on_connect
+ else: # if SENSOR_PIN of channel != 1 or low
+   print now.strftime('%Y-%m-%d %H:%M:%S') + " : " + (MQTT_PATH_STATE_INPUT + str(channel) + ': OFF')
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/stateText", "OFF", hostname=MQTT_SERVER)
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/state", "0", hostname=MQTT_SERVER)
+   publish.single(MQTT_PATH_STATE_INPUT + str(channel) + "/timestamp_OFF", now.strftime('%Y-%m-%d %H:%M:%S'), hostname=MQTT_SERVER)
+
+   time.sleep(SleepTimeL);
+
+
+# ============================
+
+
+##
+# setup GPIOS
+##
+GPIO.setmode(GPIO.BOARD)
+with open('/opt/loxberry/config/plugins/gpio/pluginconfig.json') as json_pcfg_file:
+    pcfg = json.load(json_pcfg_file)
+    gpio = pcfg['gpio']
+
+    # configure inputs
+    inputs = gpio['inputs']
+    for i in range(1, int(inputs['count'])):
+        key = "channel_{}".format(i)
+        channel = inputs[key]
+
+        wireing = GPIO.PUD_UP
+        if channel['wiring'] == 'd':
+            wireing = GPIO.PUD_DOWN
+
+        GPIO.setup(int(channel['pin']), GPIO.IN, pull_up_down = wireing) # TODO: catch runtime errors while setting configuration
+        GPIO.add_event_detect(int(channel['pin']) , GPIO.BOTH, callback=callback_input)
+
+    # configure outputs
+    outputs = gpio['outputs']
+    for i in range(1, int(outputs['count'])):
+        key = "channel_{}".format(i)
+        channel = outputs[key]
+
+        GPIO.setup(int(channel['pin']), GPIO.OUT)
+
+# ============================
+
+
+
+
+
+
+#def main():
+    # while 1:
+    #     print ("while loop active")
+    #     time.sleep(10);
+
+client = mqtt.Client()
+# client.on_connect = on_connect
  # client.on_message = on_message
-# client.connect("localhost", 1883, 60)
-# client.loop_forever()
+client.connect("localhost", 1883, 60)
+client.loop_forever()
 
 
 if __name__ == '__main__':
     try:
-        GPIO.add_event_detect(7 , GPIO.BOTH, callback=callback_input)
+
         main()
     except KeyboardInterrupt:
 		GPIO.cleanup()
