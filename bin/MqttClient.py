497e9a7e84bbd9567d0f5f4dea4a2aac4115959b
Jörg Siedhof <jsiedhof@siedhof.de>
2021-06-20 19:52:53 +0200

    Refactor

--- bin/MqttClient.py
+++ bin/MqttClient.py
@@ -7,16 +7,24 @@ import json
 import paho.mqtt.client as mqtt
 
 
-class MqttClient(mqtt.Client):
+class MqttClient():
     
     MQTT_DEFAULT_PORT = 1883
+    client = None
+    mqtt_heatbeatThread = None
     
     #Configuration MQTT Topics
-    hostname = None 
+    hostname = socket.gethostname() 
     MQTT_TOPIC_OUTPUT  = hostname + "/gpio/set/"
     MQTT_RESPONSE_STATE = hostname + "/gpio/"
     
     _LOGGER = None
+
+    #=============================
+    # publish MQTT
+    ##
+    def publish(self, topic, payload=None, qos=0, retain=False, properties=None):
+        self.client.publish(topic, payload=payload, qos=qos, retain=retain, properties=properties)
 
     # ============================
     ##
@@ -65,25 +73,25 @@ class MqttClient(mqtt.Client):
     #
     def mqtt_heatbeat(self, name):
         while(1):
-            self.publish(MqttClient.MQTT_RESPONSE_STATE+'status', "Online", retain = True)
+            self.client.publish(MqttClient.MQTT_RESPONSE_STATE+'status', "Online", retain = True)
             time.sleep(10)
 
     # ============================
     ##
     # definition of mqtt callbacks
     ##
-    def on_connect(self, client, userdata, flags, rc):
-        self.subscribe(MqttClient.MQTT_TOPIC_OUTPUT + "#")
+    def on_connectCallback(self, client, userdata, flags, rc):
+        client.subscribe(MqttClient.MQTT_TOPIC_OUTPUT + "#")
         
 
-    def on_message(self, client, userdata, msg):
+    def on_messageCallback(self, client, userdata, msg):
         mymsg = str(msg.payload.decode("utf-8"))
         mytopic = str(msg.topic)
         self._LOGGER.debug("Topic: " + mytopic + " with Payload: " + mymsg + "!")
 
         if(mytopic.startswith(MqttClient.MQTT_TOPIC_OUTPUT + "json")):
             if(not mymsg):
-                self.publish(MqttClient.MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
+                self.client.publish(MqttClient.MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
                 self._LOGGER.error('If you use json to set outputs, you have to transmit a json like {"5":"off","6":"on"}. For more information read the manual!')
                 return
             try:
@@ -95,7 +103,7 @@ class MqttClient(mqtt.Client):
                 self._LOGGER.exception("Malformed json given. Cannot parse string: " + mymsg)
             except Exception as e:
                 self._LOGGER.exception(str(e))
-                self.publish(MqttClient.MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
+                self.client.publish(MqttClient.MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
 
         # Search for topic in List of available output pins (BCM names) and set gpio to state LOW or HIGH
         for i in range(0, 27):
@@ -104,12 +112,20 @@ class MqttClient(mqtt.Client):
                     OutputChannel.handle_setOutput(i, mymsg)
                 except Exception as e:
                     self._LOGGER.exception(str(e))
-                    self.publish(MqttClient.MQTT_RESPONSE_STATE + str(i) + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
+                    self.client.publish(MqttClient.MQTT_RESPONSE_STATE + str(i) + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
 
+    def loop_forever(self):
+            self.client.loop_forever()
 
+    def disconnect(self):
+        self.client.disconnect()   
+        self.client.loop_stop() 
+        self.mqtt_heatbeat._stop()
 
     def __init__(self, configfile, _LOGGER):
-        mqttconf = self.createMqttConfig(configfile, _LOGGER)
+        self._LOGGER = _LOGGER
+        self.client = mqtt.Client()
+        mqttconf = self.createMqttConfig(configfile)
         MqttClient.hostname = socket.gethostname()
 
         if mqttconf is None:
@@ -117,11 +133,11 @@ class MqttClient(mqtt.Client):
 
         _LOGGER.info("start MQTT Client")
 
-        super().on_connect = self.on_connect
-        super().on_message = self.on_message
-        self.username_pw_set(mqttconf['username'], mqttconf['password'])
-        self.will_set(MqttClient.MQTT_RESPONSE_STATE+'status', 'Offline', qos=0, retain=True)
-        self.connect(mqttconf['address'], mqttconf['port'], 60)
+        self.client.on_connect = self.on_connectCallback
+        self.client.on_message = self.on_messageCallback
+        self.client.username_pw_set(mqttconf['username'], mqttconf['password'])
+        self.client.will_set(MqttClient.MQTT_RESPONSE_STATE+'status', 'Offline', qos=0, retain=True)
+        self.client.connect(mqttconf['address'], mqttconf['port'], 60)
 
         mqtt_heatbeatThread = threading.Thread(target=self.mqtt_heatbeat, args=(1,))
         mqtt_heatbeatThread.start()
--- bin/gpio2mqtt.py
+++ bin/gpio2mqtt.py
@@ -8,28 +8,15 @@
 ##			pip install rpi.gpio
 ## 			pip install paho-mqtt
 
-from bin.MqttClient import MqttClient
-from Channel import Channel, OutputChannel
-import paho.mqtt.client as mqtt
-import time
-import threading
+from MqttClient import MqttClient
+from Channel import Channel
+
 from datetime import datetime
 import RPi.GPIO as GPIO
-import json
 import logging
 
 import getopt
 import sys
-import socket
-
-SleepTimeL = 0.2
-
-#Configuration MQTT Topics
-hostname = socket.gethostname()
-MQTT_TOPIC_OUTPUT  = hostname + "/gpio/set/"
-MQTT_RESPONSE_STATE = hostname + "/gpio/"
-
-
 
 # ======================
 ##
@@ -95,61 +82,10 @@ _LOGGER = setup_logger("GPIO2MQTT")
 _LOGGER.debug("logfile: " + logfileArg)
 _LOGGER.info("loglevel: " + logging.getLevelName(_LOGGER.level))
 
-
-
+# ============================
+# init MQTT
+# ============================
 client = MqttClient(lbhomedir + '/data/system/plugindatabase.json', _LOGGER)
-
-
-# ============================
-##
-# get configuration from mqtt broker and store config in mqttconf variable
-##
-# mqttconf = None;
-# try:
-#     with open(lbhomedir + '/data/system/plugindatabase.json') as json_plugindatabase_file:
-#         plugindatabase = json.load(json_plugindatabase_file)
-#         mqttconfigdir = plugindatabase['plugins']['07a6053111afa90479675dbcd29d54b5']['directories']['lbpconfigdir']
-
-#         mqttPluginconfig = None
-#         with open(mqttconfigdir + '/mqtt.json') as json_mqttconfig_file:
-#             mqttPluginconfig = json.load(json_mqttconfig_file)
-
-#         mqttcred = None
-#         with open(mqttconfigdir + '/cred.json') as json_mqttcred_file:
-#             mqttcred = json.load(json_mqttcred_file)
-
-#         mqttuser = mqttcred['Credentials']['brokeruser']
-#         mqttpass = mqttcred['Credentials']['brokerpass']
-#         mqttaddressArray = mqttPluginconfig['Main']['brokeraddress'].split(":")
-#         mqttPort = MQTT_DEFAULT_PORT
-#         if len(mqttaddressArray) > 1:
-#             mqttPort = int(mqttaddressArray[1])
-
-#         mqttconf = {
-#             'username':mqttuser,
-#             'password':mqttpass,
-#             'address': mqttaddressArray[0],
-#             'port': mqttPort
-#         }
-#     _LOGGER.debug("MQTT config" + str(mqttconf))
-# except Exception as e:
-#     _LOGGER.exception(str(e))
-
-# If no mqtt config found leave the script with log entry
-if mqttconf is None:
-    _LOGGER.critical("No MQTT config found. Daemon stop working")
-    sys.exit(-1)
-
-
-# ============================
-##
-# MQTT Heartbeat
-#
-# def mqtt_heatbeat(name):
-#     while(1):
-#         client.publish(MQTT_RESPONSE_STATE+'status', "Online", retain = True)
-#         time.sleep(10)
-
 
 # ============================
 ##
@@ -157,62 +93,13 @@ if mqttconf is None:
 ##
 Channel.init(configfile, _LOGGER, client)
 
-# ============================
-##
-# definition of mqtt callbacks
-##
-# def on_connect(client, userdata, flags, rc):
-#     client.subscribe(MQTT_TOPIC_OUTPUT + "#")
-
-# ============================
-# def on_message(client, userdata, msg):
-#     mymsg = str(msg.payload.decode("utf-8"))
-#     mytopic = str(msg.topic)
-#     _LOGGER.debug("Topic: " + mytopic + " with Payload: " + mymsg + "!")
-
-#     if(mytopic.startswith(MQTT_TOPIC_OUTPUT + "json")):
-#         if(not mymsg):
-#             client.publish(MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
-#             _LOGGER.error('If you use json to set outputs, you have to transmit a json like {"5":"off","6":"on"}. For more information read the manual!')
-#             return
-#         try:
-#             msg_json = json.loads(mymsg)
-#             for key in msg_json:
-#                 OutputChannel.handle_setOutput(key, msg_json[key])
-#             return
-#         except json.decoder.JSONDecodeError as ex:
-#             _LOGGER.exception("Malformed json given. Cannot parse string: " + mymsg)
-#         except Exception as e:
-#             _LOGGER.exception(str(e))
-#             client.publish(MQTT_RESPONSE_STATE + "error" + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
-
-# 	# Search for topic in List of available output pins (BCM names) and set gpio to state LOW or HIGH
-#     for i in range(0, 27):
-#         if mytopic == MQTT_TOPIC_OUTPUT + str(i) :
-#             try:
-#                 OutputChannel.handle_setOutput(i, mymsg)
-#             except Exception as e:
-#                 _LOGGER.exception(str(e))
-#                 client.publish(MQTT_RESPONSE_STATE + str(i) + "/stateText", "Error, can't set GPIO. For more information read the logfile!", retain = True)
-
+Channel.sendChannelStates()
 # ============================
 ##
 # start mqtt Client
 ##
 try:
     _LOGGER.info("start MQTT Client")
-
-    
-    # client.on_connect = on_connect
-    # client.on_message = on_message
-    # client.username_pw_set(mqttconf['username'], mqttconf['password'])
-    # client.will_set(MQTT_RESPONSE_STATE+'status', 'Offline', qos=0, retain=True)
-    # client.connect(mqttconf['address'], mqttconf['port'], 60)
-
-#     mqtt_heatbeatThread = threading.Thread(target=mqtt_heatbeat, args=(1,))
-#     mqtt_heatbeatThread.start()
-# #    send_state()
-#     Channel.sendChannelStates()
 
     client.loop_forever()
 except Exception as e:
@@ -226,7 +113,6 @@ except Exception as e:
 finally:
     _LOGGER.info("Stop MQTT Client")
     client.disconnect() # disconnect gracefully
-    client.loop_stop() # stops network loop
     GPIO.cleanup()
     logging.shutdown()
 # ============================
